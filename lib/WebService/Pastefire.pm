c7481f3f0fc49be6e631623d7ff6628c1cf774e7
delphinus35 <delphinus@remora.cx>
2012-09-05 21:23:15 +0900

    initial commit

---
+++ lib/WebService/Pastefire.pm
@@ -0,0 +1,32 @@
+package WebService::Pastefire;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+WebService::Pastefire -
+
+=head1 SYNOPSIS
+
+  use WebService::Pastefire;
+
+=head1 DESCRIPTION
+
+WebService::Pastefire is
+
+=head1 AUTHOR
+
+JINNOUCHI Yasushi E<lt>delphinus@remora.cxE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
